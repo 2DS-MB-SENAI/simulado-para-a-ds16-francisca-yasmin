1a09ce4f16d0f567a9a80aa0570dcd76f9dea052
Francisca Yasmin Freitas de Almeida <47915176870@>
2025-04-07 10:28:50 -0300

    task1

--- livros/models.py
+++ livros/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-from django.forms import IntegerField
-
 
 class Biblioteca(models.Model):
 	titulo = models.CharField(max_length=50)
 	autor = models.CharField(max_length=30)
-	paginas = IntegerField()
+	paginas = models.IntegerField()
 		
 	def __str__(self):
 		return self.titulo
